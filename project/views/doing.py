f6372c0d931bcc2be5421d5586b13e2ab3fe3286
github ikenoue-ryo <ikenoue.ryo@gmail.com>
2020-05-31 22:48:05 +0900

    天気予報api追加

--- project/views/doing.py
+++ project/views/doing.py
@@ -1,8 +1,28 @@
+import requests
+import os
+
 from project.DB import db
 from project.views import questions
 
 def tenki_api():
-     print('天気は晴れです')
+     city_name = input('入力例: Tokyo\n')
+     app_id = 'API KYE'
+     URL = "https://api.openweathermap.org/data/2.5/weather?q={0},jp&units=metric&lang=ja&appid={1}".format(city_name, app_id)
+
+     response = requests.get(URL)
+     data = response.json()
+
+     weather = data["weather"][0]["description"]  # 最高気温
+     temp_max = data["main"]["temp_max"]  # 最低気温
+     temp_min = data["main"]["temp_min"]  # 寒暖差
+     diff_temp = temp_max - temp_min  # 湿度
+     humidity = data["main"]["humidity"]
+
+     context = {"天気": weather, "最高気温": str(temp_max) + "度", "最低気温": str(temp_min) + "度",
+                "湿度": str(humidity) + "%"}
+     print("--{0}'s Weather--".format(city_name))
+     for k, v in context.items():
+          print("{0}:{1}".format(k, v))
 
 
 def friends_api():
@@ -12,14 +32,13 @@ def friends_api():
           print(i, person.user_name)
 
      user_id = input()
-     # for person in persons:
-     #      print(person.user_name, person.age, person.nickname, person.animal)
      persons = db.session.query(db.Person).filter_by(id=user_id).first()
-     print('名前：'+ persons.user_name + '\n',
-          '年齢：'+ persons.age + '\n',
-          'ニックネーム：'+ persons.nickname + '\n',
-          '好きな動物：'+ persons.animal + '\n',
-     )
+     context = {'名前：': persons.user_name, '年齢：': persons.age,
+                'ニックネーム：': persons.nickname, '好きな動物：': persons.animal
+                }
+     for k, v in context.items():
+          print('{0}:{1}'.format(k, v))
+
 
 def others():
      print('考え中です。')
