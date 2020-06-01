596e4ee0cbdaf78915c68db3cca2e9fb5e05e34b
Ryo <ryo@RyonoMacBook-Pro.local>
2020-06-01 23:46:55 +0900

    READMEの説明追加

--- project/main.py
+++ project/main.py
@@ -2,6 +2,5 @@ import project.controller.question
 project.controller.question.question_to_you()
 project.controller.question.what_are_you_doing()
 
-
 # import project.views.doing
-# project.views.doing.gnavi_api("すし")
+# project.dviews.doing.gnavi_api("すし")
